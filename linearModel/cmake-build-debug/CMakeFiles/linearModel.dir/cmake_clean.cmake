c9c5a962464f93a2aed6d691612accade191b50e
mensaochun <953558652@qq.com>
2017-11-28 04:23:01 -0800

    init

---
+++ linearModel/cmake-build-debug/CMakeFiles/linearModel.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/linearModel.dir/main.cpp.o"
+  "linearModel.pdb"
+  "linearModel"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/linearModel.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ linearModel/cmake-build-debug/CMakeFiles/linearModel.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for linearModel.
+# This may be replaced when dependencies are built.
