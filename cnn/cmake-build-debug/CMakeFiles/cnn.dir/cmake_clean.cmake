36cf08e2f2711187aa05af4baf69dfbb9fab04c2
mensaochun <953558652@qq.com>
2017-12-02 06:46:53 -0800

    add cnn

---
+++ cnn/cmake-build-debug/CMakeFiles/cnn.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cnn.dir/main.cpp.o"
+  "cnn.pdb"
+  "cnn"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cnn.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
