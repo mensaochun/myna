b26736b3ed9d05b2b802eed0b88030b78d1428a2
yourenchun <953558652@qq.com>
2018-03-05 08:07:09 +0800

    modify math_array.h

--- mat/main.cpp
+++ mat/main.cpp
@@ -1,5 +1,15 @@
 #include "mat_array.h"
 int main() {
-	test_operator_bracket();
+    //test_operator_brackets();
+    //test_operator_mul_1();
+    //test_sum();
+    //test_mean();
+    //test_matMul();
+    //test_max();
+    Mat<int> M(10000,30000);
+    M.print();
+    //test_min();
+
+
 
 }
