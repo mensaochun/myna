381d3d11add6e115371c85534db72a2ebcd14c3b
mensaochun <953558652@qq.com>
2018-03-20 23:12:00 +0800

    fix some bugs

--- fc_vec/activation.hpp
+++ fc_vec/activation.hpp
@@ -7,36 +7,41 @@
 
 #include "mat/mat.hpp"
 #include "utils.hpp"
+
 using namespace myna;
 
 // class Activator---------------------
 
-class Activator{
+class Activator {
 public:
-    virtual Mat<double> forward(const Mat<double> &in,Mat<double> &out);
-    virtual Mat<double> backward(const Mat<double> &in,Mat<double> &out);
+    virtual Mat<double> forward(const Mat<double> &in, Mat<double> &out);
+
+    virtual Mat<double> backward(const Mat<double> &in, Mat<double> &out);
 };
 
 
 // sigmoid
-class SigmoidActivator:public Activator{
-    virtual Mat<double> forward(const Mat<double> &in,Mat<double> &out);
-    virtual Mat<double> backward(const Mat<double> &in,Mat<double> &out);
+class SigmoidActivator : public Activator {
+    virtual Mat<double> forward(const Mat<double> &in, Mat<double> &out);
+
+    virtual Mat<double> backward(const Mat<double> &in, Mat<double> &out);
 };
 
 
 // Activator-----------------------
-Mat<double> Activator::forward(const Mat<double> &in,Mat<double> &out){};
-Mat<double> Activator::backward(const Mat<double> &in,Mat<double> &out){};
+Mat<double> Activator::forward(const Mat<double> &in, Mat<double> &out) {};
+
+Mat<double> Activator::backward(const Mat<double> &in, Mat<double> &out) {};
 
 // SigmoidActivator----------------
-Mat<double> SigmoidActivator::forward(const Mat<double> &in,Mat<double> &out){
-    sigmoid(in,out);
+Mat<double> SigmoidActivator::forward(const Mat<double> &in, Mat<double> &out) {
+    sigmoid(in, out);
 };
-Mat<double> SigmoidActivator::backward(const Mat<double> &in,Mat<double> &out){
+
+Mat<double> SigmoidActivator::backward(const Mat<double> &in, Mat<double> &out) {
     Mat<double> s;
-    sigmoid(in,s);
-    out=s*(1.0-s);//copy memory.
+    sigmoid(in, s);
+    out = s * (1.0 - s);//copy memory.
 };
 
 
--- fc_vec/main.cpp
+++ fc_vec/main.cpp
@@ -1,7 +1,17 @@
 #include "solver.hpp"
 #include <ctime>
 using namespace myna;
+// help function:print()
 
+void printMat(Mat<double> X){
+    for(int i=0;i<X.getRows();i++){
+        for(int j=0;j<X.getCols();j++){
+            std::cout<<X[i][j]<<" ";
+        }
+        std::cout<<std::endl;
+    }
+    std::cout<<std::endl;
+}
 int main(){
 
     // prepare data
@@ -12,9 +22,9 @@ int main(){
 
     // construct layers
     net.addInputLayer(X);
-    net.addFullyConnectedLayer(6,5);
-    net.addFullyConnectedLayer(6,6);
-    net.addFullyConnectedLayer(6,6);
+    net.addFullyConnectedLayer(5,6,new SigmoidActivator);
+    net.addFullyConnectedLayer(6,6,new SigmoidActivator);
+    net.addFullyConnectedLayer(6,6,new SigmoidActivator);
     net.addSigmoidLayer(6,5,Y);
 
     // run solver for optimation
